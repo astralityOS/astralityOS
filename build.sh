bc8b811191669e5f23cae4adc48908d52e14b29f
Ethan Joseph <ethan.n.joseph@protonmail.com>
2022-07-28 19:46:07 -0400

    minor name fixes, last commit of today

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Main script that will call all of the other scripts
 
-ABSPATH=$(cd "$(dirname "$BASH_SOURCE[0]")"; pwd -P) # Get directory of current script
-CONFIGPATH="${ABSPATH}/config"
-fetchKernelScriptPath="${ABSPATH}/Scripts/Kernel/fetchkernel.sh"
+ROOT=$(cd "$(dirname "$BASH_SOURCE[0]")"; pwd -P) # Get directory of current script
+CONFIGPATH="${ROOT}/config"
+fetchKernelScriptPath="${ROOT}/Scripts/Kernel/fetchkernel.sh"
 
 # Make sure config exists
 
@@ -16,7 +16,7 @@ fi
 
 # Make sure device config exists
 
-if test -f "$ABSPATH/Devices/$TARGET_VENDOR/$TARGET_DEVICE.device"; then
+if test -f "$ROOT/Devices/$TARGET_VENDOR/$TARGET_DEVICE.device"; then
     echo "found device config"
 else 
     echo "Device Config File Not Found, Exiting!"
@@ -26,7 +26,7 @@ fi
 # Make sure kernel fetch script exists, and if so, execute it
 if test -f "$fetchKernelScriptPath"; then
     # Execute the kernel fetch script with the arguments of the device file and the directory root (probably not needed)
-    /bin/bash "$fetchKernelScriptPath" "$ABSPATH/Devices/$TARGET_VENDOR/$TARGET_DEVICE.device" "$ABSPATH"
+    /bin/bash "$fetchKernelScriptPath" "$ROOT/Devices/$TARGET_VENDOR/$TARGET_DEVICE.device" "$ROOT"
 else 
     echo "Kernel Fetch Script Not Found! Exiting!"
     exit 0
