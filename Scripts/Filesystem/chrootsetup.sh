73212c297505908ae28d80164c34925ae11fdbd1
buddyjojo <buddyjojo@outlook.com>
2022-07-28 20:46:20 -0400

    wip full-ish setup

--- Scripts/Filesystem/chrootsetup.sh
+++ Scripts/Filesystem/chrootsetup.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-export packages="neofetch"
-#for installing special packages/ui
-#i'm not sure how veriables can be transfered between scripts, figure that out
+#NOTE: a bit wip
 
 /debootstrap/debootstrap --second-stage
 
-#TODO: do setup steps in here: https://www.debian.org/releases/bullseye/arm64/apds03.en.html
+cat > /etc/apt/sources.list <<EOF
+deb-src http://deb.debian.org/debian $suite main
+
+deb http://deb.debian.org/debian $suite-updates main
+deb-src http://deb.debian.org/debian $suite-updates main
+EOF
+
+if [[ $suite == *"bullseye"* ]]; then
+
+cat > /etc/apt/sources.list <<EOF
+deb http://deb.debian.org/debian-security/ bullseye-security main
+deb-src http://deb.debian.org/debian-security/ bullseye-security main
+EOF
+
+else
+
+cat > /etc/apt/sources.list <<EOF
+deb http://deb.debian.org/debian-security/ $suite/updates main
+deb-src http://deb.debian.org/debian-security/ $suite/updates main
+EOF
+
+fi
+
+apt-get update
+
+#we should give some sort of option for this idk perhaps
+# tasksel install standard
 
 apt-get install -y $packages
+
+adduser --disabled-password --shell /bin/bash --home /home/$username --gecos "" $username
+
+echo "$username:$password" | chpasswd
+
+echo $hostname > /etc/hostname
+
+apt clean
 
 exit 
